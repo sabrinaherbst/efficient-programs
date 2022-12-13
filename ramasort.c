99444652642ff31036830d76c025ba11bb62f60f
EP2022 11807863 <ep11807863@g0.complang.tuwien.ac.at>
2022-12-13 15:51:52 +0100

    adds documentation for ramasort

--- ramasort.c
+++ ramasort.c
@@ -13,11 +13,13 @@
 #include <malloc.h>
 #include <assert.h>
 
+// defines entry struct that consists of int k and l and a long vlaue
 struct entry {
   int k,l;
   long value;
 };
 
+// function that takes to pointers to entries - checks if the first value is bigger (1), equal (0) or smaller (-1) than the second
 int comp_entry(const void *p1, const void *p2)
 {
   const struct entry *e1 = p1;
@@ -28,6 +30,7 @@ int comp_entry(const void *p1, const void *p2)
     return e1->value > e2->value;
 }
 
+// computes the cube of a long
 long cube(long n)
 {
   return n*n*n;
@@ -52,19 +55,27 @@ int main(int argc, char **argv)
   
   if (argc != 2)
     goto usage;
+  // converts str to integer number with base 10
   n = strtol(argv[1],&endptr,10);
   if (*endptr != '\0')
     goto usage;
+  
   table_size = size_table(n);
+  // allocates memory for the table
   table = calloc(table_size,sizeof(struct entry));
 
+  // instantiates the table with the required structs
+  // TODO: store cube(i) and cube(j) to not calculate it that often
   for (i=0; cube(i)<=n; i++) {
     for (j=i+1; cube(i)+cube(j)<=n; j++) {
+      // cretae new entry and store it in the table
       table[m++] = (struct entry){i,j,cube(i)+cube(j)};
     }
   }
   assert(m<=table_size);
+  // sort table
   qsort(table, m, sizeof(struct entry), comp_entry);
+  // go over table and see how often previous entry is the same as current
   for (i=1; i<m; i++) {
     if (table[i-1].value==table[i].value) {
       count++;
