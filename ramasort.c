8b42f6b4b4da93e4f278fa1362811f8164923ef8
Florian Schabasser <florian.schabasser@alumni.tuwien.ac.at>
2022-12-18 17:32:57 +0100

    line indents;

--- ramasort.c
+++ ramasort.c
@@ -15,80 +15,78 @@
 
 // defines entry struct that consists of int k and l and a long vlaue
 struct entry {
-  int k,l;
-  long value;
+    int k, l;
+    long value;
 };
 
 // function that takes to pointers to entries - checks if the first value is bigger (1), equal (0) or smaller (-1) than the second
-int comp_entry(const void *p1, const void *p2)
-{
-  const struct entry *e1 = p1;
-  const struct entry *e2 = p2;
-  if (e1->value < e2->value)
-    return -1;
-  else
-    return e1->value > e2->value;
+int comp_entry(const void *p1, const void *p2) {
+    const struct entry *e1 = p1;
+    const struct entry *e2 = p2;
+    if (e1->value < e2->value)
+        return -1;
+    else
+        return e1->value > e2->value;
 }
 
 // computes the cube of a long
-long cube(long n)
-{
-  return n*n*n;
+long cube(long n) {
+    return n * n * n;
 }
 
 size_t size_table(long n)
 /* compute a table size that is large enough to keep all I^3+J^3<n */
 {
-  return exp(log((double)n)*(2.0/3.0))/2+100;
+    return exp(log((double) n) * (2.0 / 3.0)) / 2 + 100;
 }
 
-int main(int argc, char **argv)
-{
-  long n;
-  char *endptr;
-  long i,j;
-  long count=0;
-  struct entry *table;
-  size_t table_size;
-  long m=0;
-  long checksum = 0;
-  
-  if (argc != 2)
-    goto usage;
-  // converts str to integer number with base 10
-  n = strtol(argv[1],&endptr,10);
-  if (*endptr != '\0')
-    goto usage;
-  
-  table_size = size_table(n);
-  // allocates memory for the table
-  table = calloc(table_size,sizeof(struct entry));
+int main(int argc, char **argv) {
+    long n;
+    char *endptr;
+    long i, j;
+    long count = 0;
+    struct entry *table;
+    size_t table_size;
+    long m = 0;
+    long checksum = 0;
 
-  // instantiates the table with the required structs
-  // TODO: store cube(i) and cube(j) to not calculate it that often
-  for (i=0; cube(i)<=n; i++) {
-    for (j=i+1; cube(i)+cube(j)<=n; j++) {
-      // cretae new entry and store it in the table
-      table[m++] = (struct entry){i,j,cube(i)+cube(j)};
-    }
-  }
-  assert(m<=table_size);
-  // sort table
-  qsort(table, m, sizeof(struct entry), comp_entry);
-  // go over table and see how often previous entry is the same as current
-  for (i=1; i<m; i++) {
-    if (table[i-1].value==table[i].value) {
-      count++;
-      checksum += table[i].value;
-      while (i<m-1 && table[i].value==table[i+1].value)
-        i++;
-    }
-  }
-  printf("%ld Ramanujan numbers up to %ld, checksum=%ld\noccupation=%ld, size=%ld\n",count,n,checksum,m,table_size);
-  printf("Memory usage: >=%ld\n",table_size*sizeof(struct entry));
-  return 0;
+    if (argc != 2)
+        goto usage;
+    // converts str to integer number with base 10
+    n = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0')
+        goto usage;
 
- usage:
-  fprintf(stderr,"usage: %s <n>\n",argv[0]);
-  exit(1);
+    table_size = size_table(n);
+    // allocates memory for the table
+    table = calloc(table_size, sizeof(struct entry));
+
+    // instantiates the table with the required structs
+    // TODO: store cube(i) and cube(j) to not calculate it that often
+    for (i = 0; cube(i) <= n; i++) {
+        for (j = i + 1; cube(i) + cube(j) <= n; j++) {
+            // cretae new entry and store it in the table
+            table[m++] = (struct entry) {i, j, cube(i) + cube(j)};
+        }
+    }
+    assert(m <= table_size);
+    // sort table
+    qsort(table, m, sizeof(struct entry), comp_entry);
+    // go over table and see how often previous entry is the same as current
+    for (i = 1; i < m; i++) {
+        if (table[i - 1].value == table[i].value) {
+            count++;
+            checksum += table[i].value;
+            while (i < m - 1 && table[i].value == table[i + 1].value)
+                i++;
+        }
+    }
+    printf("%ld Ramanujan numbers up to %ld, checksum=%ld\noccupation=%ld, size=%ld\n", count, n, checksum, m,
+           table_size);
+    printf("Memory usage: >=%ld\n", table_size * sizeof(struct entry));
+    return 0;
+
+    usage:
+    fprintf(stderr, "usage: %s <n>\n", argv[0]);
+    exit(1);
 }
