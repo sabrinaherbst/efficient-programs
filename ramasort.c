efef12c9ddc28aa0d8d6e879a7d2f480181a4c4d
soe-tu <e11809642@student.tuwien.ac.at>
2022-12-27 14:09:31 +0100

    adds debug print statements

--- ramasort.c
+++ ramasort.c
@@ -12,6 +12,7 @@
 #include <math.h>
 #include <malloc.h>
 #include <assert.h>
+#include <stdbool.h>
 
 // defines entry struct that consists of int k and l and a long value
 struct entry {
@@ -44,8 +45,7 @@ size_t size_table(long n)
 // First subarray is arr[l..m]
 // Second subarray is arr[m+1..r]
 void merge(long *arr, long l,
-           long m, long r)
-{
+           long m, long r) {
     printf("merge(%ld, %ld, %ld) \n", l, m, r);
     long i, j, k;
     long n1 = m - l + 1;
@@ -71,15 +71,11 @@ void merge(long *arr, long l,
 
     // Initial index of merged subarray
     k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
             arr[k] = L[i];
             i++;
-        }
-        else
-        {
+        } else {
             arr[k] = R[j];
             j++;
         }
@@ -96,8 +92,7 @@ void merge(long *arr, long l,
 
     // Copy the remaining elements of
     // R[], if there are any
-    while (j < n2)
-    {
+    while (j < n2) {
         arr[k] = R[j];
         j++;
         k++;
@@ -124,49 +119,69 @@ int main(int argc, char **argv) {
     if (*endptr != '\0')
         goto usage;
 
-    cbrt_n = (long)cbrt(n);
+    cbrt_n = (long) cbrt(n);
 
     table_size = size_table(n);
     // allocates memory for the table
 //    table = calloc(table_size, sizeof(struct entry));
     table = calloc(table_size, sizeof(long));
-    indices = calloc((long)cbrt(n), sizeof(long));
+    indices = calloc((long) cbrt(n), sizeof(long));
 //    printf("%ld \n", (long)cbrt(n));
 //    printf("%ld \n", table_size);
+
+    bool value_changed = true;
+    // fills the table with the values of i^3+j^3
     // instantiates the table with the required structs
     // TODO: store cube(i) and cube(j) to not calculate it that often
     for (i = 0; cube(i) <= n; i++) {
-        indices[i] = m;
+        if (value_changed) {
+            indices[i] = m;
+            value_changed = false;
+        }
         for (j = i + 1; cube(i) + cube(j) <= n; j++) {
             // create new entry and store it in the table
 //            printf("%ld %ld \n", table_size, m);
 //            table[m++] = (struct entry) {i, j, cube(i) + cube(j)};
             table[m++] = cube(i) + cube(j);
+            value_changed = true;
         }
 //        printf("%ld \n", i);
     }
+    indices[i] = m;
     printf("done \n");
     assert(m <= table_size);
 //    qsort(table, m, sizeof(struct entry), comp_entry);
+
+    for (i = 0; i < table_size; i++) {
+        printf("%ld \n", table[i]);
+    }
+
+    printf("done \n");
+
+    for (i = 0; i < table_size; i++) {
+        printf("%ld \n", table[i]);
+    }
+
+    printf("done \n");
+
+    printf("indices: \n");
+    for (int i = 0; i < m; ++i) {
+        printf("%ld \n", indices[i]);
+    }
 
     // sort table
     // 1st iteration 0, 2, 4, ...
     // 2nd iteration 0, 4, 8, 12, ...
     // 3rd iteration 0, 8, 16, 24, ...
-    int increment = 2;
-    for (i = 0; i < log2(cbrt_n); i++) {
-        for (j = 0; j < cbrt_n-1-increment; (j+=increment)) {
-//            printf("%ld %ld \n", cbrt_n, j);
-            merge(table, indices[j], indices[j+increment-1]-1, indices[j+increment]-1);
+    int increment = 1;
+    for (i = 0; i < (long) log2(cbrt_n); i++) {
+        printf("Iteration: %ld of %ld n=%ld \n", i + 1, (long) log2(cbrt_n), n);
+        for (j = 0; j < cbrt_n - 2 * increment; j += 2 * increment) {
+            merge(table, indices[j], indices[j + increment] - 1, indices[j + increment * 2] - 1);
         }
-//        printf("%ld %ld \n", cbrt_n, i);
-        merge(table, indices[cbrt_n-increment], indices[cbrt_n-increment+increment-1]-1, m-1);
         increment *= 2;
     }
 
-    for (i = 0; i < table_size; i++) {
-        printf("%ld \n", table[i]);
-    }
 
     // iterate over the table and check if the next entry is the same as the current one
     for (i = 1; i < m; i++) {
@@ -178,7 +193,7 @@ int main(int argc, char **argv) {
         }
     }
 
-//    // iterate over the table and check if the next entry is the same as the current one
+    // iterate over the table and check if the next entry is the same as the current one
 //    for (i = 1; i < m; i++) {
 //        if (table[i - 1].value == table[i].value) {
 //            count++;
