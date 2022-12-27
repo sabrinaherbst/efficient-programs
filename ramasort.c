7f0bb77df56ac34ad5d6baf29ec6bf249b16185f
soe-tu <e11809642@student.tuwien.ac.at>
2022-12-27 11:44:22 +0100

    adds structure for merge sort implementation (currently not functional)

--- ramasort.c
+++ ramasort.c
@@ -13,7 +13,7 @@
 #include <malloc.h>
 #include <assert.h>
 
-// defines entry struct that consists of int k and l and a long vlaue
+// defines entry struct that consists of int k and l and a long value
 struct entry {
     int k, l;
     long value;
@@ -40,12 +40,79 @@ size_t size_table(long n)
     return exp(log((double) n) * (2.0 / 3.0)) / 2 + 100;
 }
 
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(long *arr, long l,
+           long m, long r)
+{
+    printf("merge(%ld, %ld, %ld) \n", l, m, r);
+    long i, j, k;
+    long n1 = m - l + 1;
+    long n2 = r - m;
+
+    // Create temp arrays
+    long L[n1], R[n2];
+
+    // Copy data to temp arrays
+    // L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back
+    // into arr[l..r]
+    // Initial index of first subarray
+    i = 0;
+
+    // Initial index of second subarray
+    j = 0;
+
+    // Initial index of merged subarray
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements
+    // of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
 int main(int argc, char **argv) {
     long n;
+    long cbrt_n;
     char *endptr;
     long i, j;
     long count = 0;
-    struct entry *table;
+//    struct entry *table;
+    long *table;
+    long *indices;
     size_t table_size;
     long m = 0;
     long checksum = 0;
@@ -57,30 +124,69 @@ int main(int argc, char **argv) {
     if (*endptr != '\0')
         goto usage;
 
+    cbrt_n = (long)cbrt(n);
+
     table_size = size_table(n);
     // allocates memory for the table
-    table = calloc(table_size, sizeof(struct entry));
-
+//    table = calloc(table_size, sizeof(struct entry));
+    table = calloc(table_size, sizeof(long));
+    indices = calloc((long)cbrt(n), sizeof(long));
+//    printf("%ld \n", (long)cbrt(n));
+//    printf("%ld \n", table_size);
     // instantiates the table with the required structs
     // TODO: store cube(i) and cube(j) to not calculate it that often
     for (i = 0; cube(i) <= n; i++) {
+        indices[i] = m;
         for (j = i + 1; cube(i) + cube(j) <= n; j++) {
-            // cretae new entry and store it in the table
-            table[m++] = (struct entry) {i, j, cube(i) + cube(j)};
+            // create new entry and store it in the table
+//            printf("%ld %ld \n", table_size, m);
+//            table[m++] = (struct entry) {i, j, cube(i) + cube(j)};
+            table[m++] = cube(i) + cube(j);
         }
+//        printf("%ld \n", i);
     }
+    printf("done \n");
     assert(m <= table_size);
+//    qsort(table, m, sizeof(struct entry), comp_entry);
+
     // sort table
-    qsort(table, m, sizeof(struct entry), comp_entry);
-    // go over table and see how often previous entry is the same as current
+    // 1st iteration 0, 2, 4, ...
+    // 2nd iteration 0, 4, 8, 12, ...
+    // 3rd iteration 0, 8, 16, 24, ...
+    int increment = 2;
+    for (i = 0; i < log2(cbrt_n); i++) {
+        for (j = 0; j < cbrt_n-1-increment; (j+=increment)) {
+//            printf("%ld %ld \n", cbrt_n, j);
+            merge(table, indices[j], indices[j+increment-1]-1, indices[j+increment]-1);
+        }
+//        printf("%ld %ld \n", cbrt_n, i);
+        merge(table, indices[cbrt_n-increment], indices[cbrt_n-increment+increment-1]-1, m-1);
+        increment *= 2;
+    }
+
+    for (i = 0; i < table_size; i++) {
+        printf("%ld \n", table[i]);
+    }
+
+    // iterate over the table and check if the next entry is the same as the current one
     for (i = 1; i < m; i++) {
-        if (table[i - 1].value == table[i].value) {
+        if (table[i - 1] == table[i]) {
             count++;
-            checksum += table[i].value;
-            while (i < m - 1 && table[i].value == table[i + 1].value)
+            checksum += table[i];
+            while (i < m - 1 && table[i] == table[i + 1])
                 i++;
         }
     }
+
+//    // iterate over the table and check if the next entry is the same as the current one
+//    for (i = 1; i < m; i++) {
+//        if (table[i - 1].value == table[i].value) {
+//            count++;
+//            checksum += table[i].value;
+//            while (i < m - 1 && table[i].value == table[i + 1].value)
+//                i++;
+//        }
+//    }
     printf("%ld Ramanujan numbers up to %ld, checksum=%ld\noccupation=%ld, size=%ld\n", count, n, checksum, m,
            table_size);
     printf("Memory usage: >=%ld\n", table_size * sizeof(struct entry));
